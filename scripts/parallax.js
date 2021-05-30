b0172567ca518fae3b889144ff2178392f678749
Antony Otero <antonyotero.nyc@gmail.com>
2021-05-30 09:49:05 -0400

    Set the background color on scroll

--- scripts/parallax.js
+++ scripts/parallax.js
@@ -4,4 +4,17 @@ $(document).on("scroll", () => {
   $(".progress .counter").text("Pixels Down: " + pixelsFromTop);
 
   (pixelsFromTop > 50) ? $("header").addClass("hidden") : $("header").removeClass("hidden");
+  
+  if(pixelsFromTop < 600) {
+    $("body").css("background-color", "#fff");
+  } else if(pixelsFromTop < 1400) {
+    $("body").css("background-color", "#a29c97");
+  } else if(pixelsFromTop < 2200) {
+    $("body").css("background-color", "#d9dfe4");
+  } else if(pixelsFromTop < 3000) {
+    $("body").css("background-color", "#fff0f0");
+  } else {
+    $("body").css("background-color", "#cdccc7");
+  }
+
 });

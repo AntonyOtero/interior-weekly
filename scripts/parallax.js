9f81cb7897f70feadf1af044c89dee737b6e960e
Antony Otero <antonyotero.nyc@gmail.com>
2021-05-30 10:39:37 -0400

    Update site responsiveness

--- scripts/parallax.js
+++ scripts/parallax.js
@@ -1,20 +1,29 @@
+
 $(document).on("scroll", () => {
   let pixelsFromTop = $(document).scrollTop();
   let scrollHeight = $(document).height() - $(window).height();
   let percentScrolled = 100 * (pixelsFromTop / scrollHeight);
-
+  
+  let imageHeight = $("section img:first-of-type").height();
+  let imagePadding = [...$("section:first-of-type").css("padding-top")].splice(0,2).join("");
+  let transitionZone = imageHeight + imagePadding * 2
+  
+  function getTransitionPoint(imageNum) {
+    return (imageHeight * imageNum) + (imagePadding * 2);
+  }
+  
   $(".progress-bar").css("width", percentScrolled + "%");
   $(".progress-counter").text("Pixels Down: " + pixelsFromTop);
 
   (pixelsFromTop > 50) ? $("header").addClass("hidden") : $("header").removeClass("hidden");
   
-  if(pixelsFromTop < 600) {
+  if(pixelsFromTop < imageHeight) {
     $("body").css("background-color", "#fff");
-  } else if(pixelsFromTop < 1400) {
+  } else if(pixelsFromTop < getTransitionPoint(2)) {
     $("body").css("background-color", "#a29c97");
-  } else if(pixelsFromTop < 2200) {
+  } else if(pixelsFromTop < getTransitionPoint(3)) {
     $("body").css("background-color", "#d9dfe4");
-  } else if(pixelsFromTop < 3000) {
+  } else if(pixelsFromTop < getTransitionPoint(4)) {
     $("body").css("background-color", "#fff0f0");
   } else {
     $("body").css("background-color", "#cdccc7");
